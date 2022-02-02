049b6215cb999ef72983209799140fed8cc30fbc
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-02-02 22:10:46 +0900

    help command (garbage)

---
+++ src/cogs/help.ts
@@ -0,0 +1,7 @@
+import { GuildMember, RoleResolvable } from 'discord.js'
+import { client } from '..'
+
+client.on('messageCreate', async (message) => {
+  if (message.content.startsWith('.help')) {
+  }
+})
