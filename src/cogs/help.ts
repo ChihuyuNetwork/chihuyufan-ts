b96e8f34be25ff95e6876df9a120dda047352825
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-02-02 22:44:45 +0900

    add help command

--- src/cogs/help.ts
+++ src/cogs/help.ts
@@ -1,6 +1,21 @@
+import { MessageEmbed } from 'discord.js'
 import { client } from '..'
 
 client.on('messageCreate', async (message) => {
   if (message.content.startsWith('.help')) {
+    const cmds = [
+      `**.neko**: meow`,
+      `**.achieve**: unlock user's achievement.`,
+      `**.vc**: change joining vc name.`,
+      `**.vote**: start a vote.`,
+      `**.help**: show this.`
+    ]
+
+    const embed = new MessageEmbed()
+      .setTitle('Chihuyu fan Commands List')
+      .setColor('AQUA')
+      .setDescription(cmds.join('\n'))
+      .setTimestamp()
+    await message.channel.send({ embeds: [embed] })
   }
 })
