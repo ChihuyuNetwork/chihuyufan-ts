bb4afdcd3375df92a7bd89008a9a1d679d14d145
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-02-02 22:09:51 +0900

    add achievement command

---
+++ src/cogs/achieve.ts
@@ -0,0 +1,39 @@
+import { GuildMember, RoleResolvable } from 'discord.js'
+import { client } from '..'
+
+client.on('messageCreate', async (message) => {
+  if (message.content.startsWith('.achieve')) {
+    const args = message.content.split(' ').slice(1)
+
+    const target =
+      message.mentions.members?.first() ||
+      message.guild?.members.cache.find(
+        (m) => m.displayName === args[0] || m.user.username === args[0]
+      )
+
+    if (target === undefined) {
+      await message.channel.send('Specify user not found.')
+      return
+    }
+
+    if (args.length < 2) {
+      await message.channel.send('Please provide a role name.')
+      return
+    }
+
+    if (target.roles.cache.find((r) => r.name === args[1]) === undefined) {
+      const role = await message.guild?.roles.create({
+        name: args.slice(1).join(' ')
+      })
+      target?.roles.add(role as RoleResolvable)
+
+      await message.channel.send(
+        `${target?.displayName} has unlocked achivement: **${role?.name}**`
+      )
+    } else {
+      await message.channel.send(
+        `The user has already unlocked that achivement.`
+      )
+    }
+  }
+})
