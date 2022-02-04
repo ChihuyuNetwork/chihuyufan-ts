b2cff861409f8147824e981e5fcfaf34b6070c59
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-02-04 09:14:02 +0900

    update hlep

--- src/cogs/help.ts
+++ src/cogs/help.ts
@@ -8,6 +8,8 @@ client.on('messageCreate', async (message) => {
       `**.achieve**: ユーザーを実績を解除します`,
       `**.vc**: 参加しているVCの名前を変更します`,
       `**.vote**: 投票を開始します`,
+      `**.pr**: 文句言うな黙ってPR出せ`,
+      `**.avatar**: ユーザーのアイコンを表示します`,
       `**.help**: これ`
     ]
 
