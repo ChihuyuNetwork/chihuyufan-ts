91f62add5b98bac26358a066a61c0eed133b45a0
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-02-12 23:01:32 +0900

    敗北er

---
+++ src/cogs/lose.ts
@@ -0,0 +1,13 @@
+import { client } from '..'
+
+client.on('messageCreate', async (message) => {
+  const pattern = /(しかし|だが)\S+に絡まれ/
+  if (pattern.test(message.content)) {
+    const channel = message.channel
+    if (Math.floor(Math.random() * 101) > 2) {
+      await channel.send('敗北...')
+    } else {
+      await channel.send('勝利...')
+    }
+  }
+})
