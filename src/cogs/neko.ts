ab3eb22364bd9db0b0a72527c11a5ee8464b08a8
Mogyuchi <git@mogyuchi.com>
2022-02-13 07:17:56 +0000

    add slash command (neko) #19

--- src/cogs/neko.ts
+++ src/cogs/neko.ts
@@ -1,4 +1,19 @@
-import { client } from '..'
+import { client, guildId } from '..'
+
+client.once('ready', async () => {
+  client.application?.commands.create(
+    {
+      name: 'neko',
+      description: 'にゃー'
+    },
+    guildId
+  )
+})
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand() || interaction.commandName !== 'neko') return
+  await interaction.reply('にゃー')
+})
 
 client.on('messageCreate', async (message) => {
   if (message.content === '.neko') {
