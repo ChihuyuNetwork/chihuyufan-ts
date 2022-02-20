5911a8718af2edb8cd6008a2fd464aa029241ad5
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-02-20 22:55:01 +0900

    fix probability

--- src/cogs/iine.ts
+++ src/cogs/iine.ts
@@ -1,7 +1,7 @@
 import { client } from '..'
 
 client.on('messageCreate', async (message) => {
-  if (Math.floor(Math.random() * 10001) > 2) {
+  if (Math.floor(Math.random() * 10001) < 2) {
     await message.reply('いいね')
   }
 })
