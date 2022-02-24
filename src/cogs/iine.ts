14ec4fb95fe378b19d7989078d16288f29dae8cc
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-02-24 20:34:55 +0900

    0.05%にした

--- src/cogs/iine.ts
+++ src/cogs/iine.ts
@@ -1,6 +1,6 @@
 import { client } from '..'
 
 client.on('messageCreate', async (message) => {
-  if (Math.floor(Math.random() * 10001)) return
+  if (Math.floor(Math.random() * 2000)) return
   await message.reply('いいね')
 })
