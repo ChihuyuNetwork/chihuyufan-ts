02aa56be709dc4e7ff938b3cf56124cb32439bdd
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-02-28 00:16:27 +0900

    確率引き上げ

--- src/cogs/iine.ts
+++ src/cogs/iine.ts
@@ -1,6 +1,6 @@
 import { client } from '..'
 
 client.on('messageCreate', async (message) => {
-  if (Math.floor(Math.random() * 5000)) return
+  if (Math.floor(Math.random() * 8000)) return
   await message.reply('いいね')
 })
