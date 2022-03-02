8bc3badf393fe6975cbe8d8c342d2c60e20c65d5
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-03-03 00:17:08 +0900

    fix typo (1)

--- src/cogs/help.ts
+++ src/cogs/help.ts
@@ -36,13 +36,14 @@ client.on('interactionCreate', async (interaction) => {
 client.on('messageCreate', async (message) => {
   if (message.content === '.help') {
     const cmds = [
-      `**.neko**: にゃー`,
-      `**.achieve**: ユーザーを実績を解除します`,
-      `**.vc**: 参加しているVCの名前を変更します`,
-      `**.vote**: 投票を開始します`,
-      `**.pr**: 文句言うな黙ってPR出せ`,
-      `**.avatar**: ユーザーのアイコンを表示します`,
-      `**.help**: これ`
+      `**/neko**: にゃー`,
+      `**/achieve**: ユーザーの実績を解除します`,
+      `**/vc**: 参加しているVCの名前を変更します`,
+      `**/vote**: 投票を開始します`,
+      `**/pr**: 文句言うな黙ってPR出せ`,
+      `**/avatar**: ユーザーのアイコンを表示します`,
+      `**/dice**: ランダムに抽選します`,
+      `**/help**: これ`
     ]
 
     const embed = new MessageEmbed()
