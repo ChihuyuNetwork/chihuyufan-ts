fededc8030acc1167155fb56cae5741273c5af53
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2023-03-15 04:55:24 +0900

    登録処理省略

--- src/cogs/applicationCommands.ts
+++ src/cogs/applicationCommands.ts
@@ -3,7 +3,7 @@ import { client } from '..'
 import { guildId } from '../constant'
 
 client.once(Events.ClientReady, async () => {
-  await client.application?.fetch()
-  await client.application?.commands.set([], guildId)
-  client.emit('commandsReset')
+  // await client.application?.fetch()
+  // await client.application?.commands.set([], guildId)
+  // client.emit('commandsReset')
 })
