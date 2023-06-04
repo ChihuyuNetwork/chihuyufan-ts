61fc2f77cee94d75e4194e9caa5fcbc2687a9726
hirosukt <senolynn7584@gmail.com>
2023-06-05 07:52:25 +0900

    Update left.ts

--- src/cogs/left.ts
+++ src/cogs/left.ts
@@ -3,7 +3,7 @@ import { client } from '..'
 
 client.on(Events.GuildMemberRemove, async (member) => {
   const channel = client.guilds.cache
-    .find((g) => g.id === '840530499901849620')!
-    .channels.cache.find((c) => c.name === 'log')!
+    .find((g) => g.id === '1024353262226374686')!
+    .channels.cache.find((c) => c.name === 'left-log')!
   if (channel.isTextBased()) await channel.send('left: ' + member.displayName)
 })
