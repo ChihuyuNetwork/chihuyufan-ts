a7296548a62a5a5274abd29f22c6b09552ac7a55
hirosukt <senolynn7584@gmail.com>
2023-10-30 20:56:39 +0900

    絵文字の作成通知

---
+++ src/cogs/emojiUpdate.ts
@@ -0,0 +1,9 @@
+import {Events, TextChannel} from 'discord.js'
+import {client} from '..'
+
+client.on(Events.GuildEmojiCreate, async (emoji) => {
+    if (emoji.guild.id !== '928978742825586708') return
+    const notifyChannel = await emoji.guild.channels.fetch('928978742825586711') as TextChannel
+    await notifyChannel.send('新しい絵文字が追加されました！')
+    await notifyChannel.send(':' + emoji.name! + ':')
+})
