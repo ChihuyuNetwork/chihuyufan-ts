77edb677189ae96476b4a1ef65cf743a99f666bf
hirosukt <senolynn7584@gmail.com>
2023-10-30 20:57:57 +0900

    ステッカー通知も

--- src/cogs/emojiUpdate.ts
+++ src/cogs/emojiUpdate.ts
@@ -5,5 +5,13 @@ client.on(Events.GuildEmojiCreate, async (emoji) => {
     if (emoji.guild.id !== '928978742825586708') return
     const notifyChannel = await emoji.guild.channels.fetch('928978742825586711') as TextChannel
     await notifyChannel.send('新しい絵文字が追加されました！')
-    await notifyChannel.send(':' + emoji.name! + ':')
+    await notifyChannel.send(emoji.url)
 })
+
+client.on(Events.GuildStickerCreate, async (sticker) => {
+    if (sticker.guild!.id !== '928978742825586708') return
+    const notifyChannel = await sticker.guild!.channels.fetch('928978742825586711') as TextChannel
+    await notifyChannel.send('新しい絵文字が追加されました！')
+    await notifyChannel.send(sticker.url)
+})
+
