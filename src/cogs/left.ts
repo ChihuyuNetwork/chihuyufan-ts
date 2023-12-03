4893b43c70a7c3bc3ea0dcbe9c4f55db2d92f970
hirosukt <senolynn7584@gmail.com>
2023-12-03 18:46:20 +0900

    remove

--- src/cogs/left.ts
+++
@@ -1,9 +0,0 @@
-import { Events } from 'discord.js'
-import { client } from '..'
-
-client.on(Events.GuildMemberRemove, async (member) => {
-  const channel = client.guilds.cache
-    .find((g) => g.id === '1024353262226374686')!
-    .channels.cache.find((c) => c.name === 'left-log')!
-  if (channel.isTextBased()) await channel.send('left: ' + member.displayName)
-})
